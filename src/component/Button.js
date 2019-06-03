9546ce37a463bcf4b5d13eabdf773091cc9797f5
Pierre Lambert <supagoof924@hotmail.com>
2019-06-03 21:36:13 +0200

    [ FEATURE ] Added Context and plugged in to Button and Field Components

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,6 +1,8 @@
 import React from "react";
+import languageContext from "../contexts/languageContext";
 
 class Button extends React.Component {
+  static contextType = languageContext;
   render() {
     return <div className="ui button primary">Submit</div>;
   }
--- src/component/Field.js
+++ src/component/Field.js
@@ -1,7 +1,11 @@
 import React from "react";
+import languageContext from "../contexts/languageContext";
 
 class Field extends React.Component {
+  static contextType = languageContext;
+
   render() {
+    console.log(this.context);
     return (
       <div className="ui field">
         <label>Name</label>
